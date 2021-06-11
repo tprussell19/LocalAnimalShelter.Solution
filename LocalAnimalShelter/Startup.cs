b2fbaeb53c9aa763de7d2ddfad51a0f428b9e83c
Thomas Russell <t.p.russell19@gmail.com>
2021-06-11 14:28:41 -0700

    :wrench: add further configuration to customize swagger documentation

--- LocalAnimalShelter/Startup.cs
+++ LocalAnimalShelter/Startup.cs
@@ -1,11 +1,14 @@
+using LocalAnimalShelter.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.EntityFrameworkCore;
-using LocalAnimalShelter.Models;
 using Microsoft.OpenApi.Models;
+using System;
+using System.Reflection;
+using System.IO;
 
 namespace LocalAnimalShelter.Solution
 {
@@ -50,9 +53,30 @@ namespace LocalAnimalShelter.Solution
 
       services.AddDbContext<LocalAnimalShelterContext>(options => options
           .UseMySql(Configuration["ConnectionStrings:DefaultConnection"], ServerVersion.AutoDetect(Configuration["ConnectionStrings:DefaultConnection"])));
+
       services.AddSwaggerGen(c =>
       {
-        c.SwaggerDoc("v1", new OpenApiInfo { Title = "LocalAnimalShelter.Solution", Version = "v1" });
+        c.SwaggerDoc("v1", new OpenApiInfo
+        {
+          Title = "LocalAnimalShelter.Solution",
+          Version = "v1",
+          Description = "An ASP.NET Core Web API",
+          Contact = new OpenApiContact
+          {
+            Name = "Thomas Russell",
+            Email = "t.p.russell19@gmail.com",
+            Url = new Uri("https://github.com/tprussell19")
+          },
+          License = new OpenApiLicense
+          {
+            Name = "MIT",
+            Url = new Uri("https://opensource.org/licenses/MIT")
+          }
+        });
+
+        var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+        var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+        c.IncludeXmlComments(xmlPath);
       });
     }
 
@@ -61,11 +85,16 @@ namespace LocalAnimalShelter.Solution
       if (env.IsDevelopment())
       {
         app.UseDeveloperExceptionPage();
-        app.UseSwagger();
-        app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "LocalAnimalShelter.Solution v1"));
       }
 
       // app.UseHttpsRedirection();
+
+      app.UseSwagger();
+
+      app.UseSwaggerUI(c =>
+      {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "LocalAnimalShelter.Solution v1");
+      });
 
       app.UseRouting();
 
