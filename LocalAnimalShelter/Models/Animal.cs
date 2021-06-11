13741b735b4958f9db5094d8739f0be72192ada6
Thomas Russell <t.p.russell19@gmail.com>
2021-06-11 10:07:37 -0700

    :hammer: build animal model

---
+++ LocalAnimalShelter/Models/Animal.cs
@@ -0,0 +1,15 @@
+namespace LocalAnimalShelter.Models
+{
+  public class Animal
+  {
+    public int AnimalId { get; set; }
+    public string AnimalType { get; set; }
+    public string Name { get; set; }
+    public string Breed { get; set; }
+    public string Sex { get; set; }
+    public string Color { get; set; }
+    public string Age { get; set; }
+    public int Weight { get; set; }
+    public string Description { get; set; }
+  }
+}
