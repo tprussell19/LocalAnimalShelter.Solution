4ac585345f1622ca44d5370b184e09bb29a633ce
Thomas Russell <t.p.russell19@gmail.com>
2021-06-11 17:13:48 -0700

    :construction: add partially completed route annotations for swagger documentation

--- LocalAnimalShelter/Controllers/AnimalsController.cs
+++ LocalAnimalShelter/Controllers/AnimalsController.cs
@@ -2,16 +2,18 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
 using LocalAnimalShelter.Models;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Http;
 
 namespace LocalAnimalShelter.Controllers
 {
-  [ApiController]
+  [Produces("application/json")]
   [ApiVersion("1.0")]
   [Route("api/v{version:ApiVersion}/[controller]")]
+  [ApiController]
   public class AnimalsController : ControllerBase
   {
     private readonly LocalAnimalShelterContext _db;
@@ -26,14 +28,20 @@ namespace LocalAnimalShelter.Controllers
     /// <summary>
     /// Get a list of all of the animals available for adoption in the shelter
     /// </summary>
+    /// <response code="200">Returns all animals from the database</response>
     [HttpGet]
+    [ProducesResponseType(StatusCodes.Status200OK)]
     public async Task<ActionResult<IEnumerable<Animal>>> Get()
     => await _db.Animals.ToListAsync();
 
     /// <summary>
     /// Get the detailed information about one animal
     /// </summary>
+    /// <response code="200">Returns the requested animal</response>
+    /// <response code="404">Requested animal cannot be found in the database. Check that you have entered the correct AnimalId</response>
     [HttpGet("{id}")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
     public async Task<ActionResult<Animal>> GetAnimal(int id)
     {
       Animal a = await _db.Animals.FindAsync(id);
@@ -44,7 +52,29 @@ namespace LocalAnimalShelter.Controllers
     /// <summary>
     /// Create the information for a new animal in the shelter
     /// </summary>
+    /// <remarks>
+    /// When adding a new animal to the database, please remove the "AnimalId" key-value pair from your entry. The database will assign an id to the entry upon successful creation.
+    ///
+    /// Sample request:
+    /// ```
+    /// {
+    ///   "animalType": "Cat",
+    ///   "name": "Felix",
+    ///   "breed": "Domestic short hair",
+    ///   "sex": "male",
+    ///   "color": "Brown tabby",
+    ///   "age": "3 years",
+    ///   "weight": 15,
+    ///   "description": "A true scaredy cat"
+    /// }
+    /// ```
+    /// </remarks>
+    /// <returns>A newly created entry for an animal at the shelter</returns>
+    /// <response code="201">Returns the new animal info</response>
+    /// <response code="400">If the entry is null</response>
     [HttpPost]
+    [ProducesResponseType(StatusCodes.Status201Created)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
     public async Task<ActionResult<Animal>> Post(Animal a)
     {
       _db.Animals.Add(a);
@@ -55,6 +85,9 @@ namespace LocalAnimalShelter.Controllers
     /// <summary>
     /// Edit the information of an animal
     /// </summary>
+    /// <remarks>
+    /// Make sure that AnimalId matches the parameter id entered and be sure to fill out all fields.
+    /// </remarks>
     [HttpPut("{id}")]
     public async Task<ActionResult> Put(int id, Animal a)
     {
